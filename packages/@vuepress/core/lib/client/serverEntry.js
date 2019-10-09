b5639e9d678d89f7d753f2c9c07ae9f836bd3d8d
ciolt <5000732+ciolt@users.noreply.github.com>
2019-10-08 22:52:09 -0400

    Update serverEntry.js
    Add meta to serverEntry (do not compile)

--- packages/@vuepress/core/lib/client/serverEntry.js
+++ packages/@vuepress/core/lib/client/serverEntry.js
@@ -2,13 +2,14 @@ import { createApp } from './app'
 
 export default context => new Promise((resolve, reject) => {
   const { app, router } = createApp(true /* isServer */)
-  const { url } = context
   const { fullPath } = router.resolve(url).route
+  const meta = app.$meta()  
 
   if (fullPath !== url) {
     return reject({ url: fullPath })
   }
 
-  router.push(url)
+  router.push(context.url)
+  context.meta = meta
   router.onReady(() => resolve(app))
 })
